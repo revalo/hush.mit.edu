9b38946155e9af2b64503d92b47563ba16b553cd
Shreyas Kapur <shreyask@mit.edu>
2017-10-30 23:55:18 -0400

    Treat newlines as <br>

--- confess/__init__.py
+++ confess/__init__.py
@@ -32,6 +32,21 @@ def modify_query(**new_values):
 
     return '{}?{}'.format(request.path, url_encode(args))
 
+# To get newlines
+import re
+from jinja2 import evalcontextfilter, Markup, escape
+
+_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
+
+@app.template_filter()
+@evalcontextfilter
+def nl2br(eval_ctx, value):
+    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
+        for p in _paragraph_re.split(escape(value)))
+    if eval_ctx.autoescape:
+        result = Markup(result)
+    return result
+
 app.jinja_env.filters['datetimefilter'] = display_date
 
 from confess.models import db
